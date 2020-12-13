12330776a64c3272c03c21b52eb2ac31f2940282
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-13 23:00:10 +0300

    Added tool bar

--- src3.py
+++ src3.py
@@ -1,19 +1,25 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtWidgets import QMainWindow,QApplication,QToolBar
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Main Window App")
-        self.setGeometry(100,100,800,800)
+        self.setGeometry(100,100,800,500)
         self._createMenu()
+        self._createToolBar()
 
     def _createMenu(self):
         self.menu = self.menuBar()
         self.menu.addMenu("&Menu")
         self.menu.addAction("&Exit",self.close)
+
+    def _createToolBar(self):
+        toolBar = QToolBar()
+        self.addToolBar(toolBar)
+        toolBar.addAction("Exit",self.close)
 
 
 if __name__ == "__main__":
