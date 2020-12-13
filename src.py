64062c0194a11a81b8ac4a1bc478554238913661
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-13 22:03:16 +0300

    Played around with the 3 first layouts

--- src.py
+++ src.py
@@ -1,24 +1,55 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication,QLabel,QWidget # installing required widgets
+from PyQt5.QtWidgets import QApplication,QLabel,QWidget,QGridLayout # installing required widgets
 
 from layouts.hLayout import MyHLayout
+from layouts.vLayout import MyVLayout
 
 app = QApplication(sys.argv)
 
 class MainWindow(QWidget):
 
-    def __init__(self,layout,*args,**kwargs):
+    def __init__(self,layout,layout2,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.setWindowTitle("Layouts")
         self.setGeometry(100,100,500,500)
-        self.setLayout(layout)
+        self.mainLayout = QGridLayout()
+        self.setLayout(self.mainLayout)
+        myWidget = QWidget()
+        myWidget.setLayout(layout)
+        self.mainLayout.addWidget(myWidget,0,0)
+        myWidget2 = QWidget()
+        myWidget2.setLayout(layout2)
+        self.mainLayout.addWidget(myWidget2,0,1)
+        # self.mainLayout.addWidget(QLabel(text="Helooo"),0,0)
+
+    # def add_layouts(self,left_layout,right_layout):
+    #     self.add_left_layout(left_layout)
+    #     self.add_right_layout(right_layout)
+
+    # def add_left_layout(self,layout):
+    #     self.mainLayout.addWidget(layout,0,0)
+
+
+    # def add_right_layout(self,layout):
+    #     self.mainLayout.addWidget(layout,0,1)
+
+
+
 
 hLayout = MyHLayout()
 
 hLayout.add_widgets()
 
-window = MainWindow(hLayout)
+
+vLayout = MyVLayout()
+
+vLayout.add_widgets()
+
+
+window = MainWindow(vLayout,hLayout)
+
+# window.add_layouts(vLayout,hLayout)
 
 window.show()
 
