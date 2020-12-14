683280f91858a16b7e37429f6b3ccd90c1239084
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-14 18:28:35 +0300

    Added central widget createe basic logic

--- db/src2.py
+++ db/src2.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication,QMainWindow,QMessageBox)
+from PyQt5.QtWidgets import (QApplication,QMainWindow,QMessageBox,QTableWidgetItem,QTableWidget)
 from PyQt5.QtSql import QSqlDatabase,QSqlQuery
 
 
@@ -11,6 +11,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(100,100,400,400)
         self.setWindowTitle("Database GUI App")
         self.createDbConnection()
+        self.createEmployeeTable()
 
 
     def createDbConnection(self):
@@ -19,6 +20,33 @@ class MainWindow(QMainWindow):
         if not self.con.open():
             QMessageBox.critical(None,"Database Error!",f"{con.lastError().databaseText()}")
             sys.exit()
+
+    def checkIfTableExists(self,tableName):
+        tables = self.con.tables()
+        return tableName in tables
+
+
+    def createEmployeeTable(self):
+        if not self.checkIfTableExists("employee"):
+            queryString = """
+                CREATE TABLE employee (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+                    name VARCHAR(40) NOT NULL,
+                    job VARCHAR(50),
+                    email VARCHAR(40) NOT NULL,
+                    phone VARCHAR(20),
+                    age INTEGER
+                )
+            """
+            query = QSqlQuery()
+            query.exec(queryString)
+
+    
+    def createCentralWidget(self):
+        self.view = QTableWidget()
+        self.view.setColumnCount(6)
+        self.view.setHorizontalHeaderLabels(["ID", "Name", "Job", "Email","Phone","Age"])
+        query = QSqlQuery("SELECT id, name, job, email,phone,age FROM employee")
 
 
 
